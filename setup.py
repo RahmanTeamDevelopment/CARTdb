dc977368defc58376cc5b9e0a46fc82d3492399d
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-10-06 02:45:57 +0200

    Changes in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name= 'CARTdb',
     version = '0.1.0',
-    description = 'blabla',
+    description = 'blaCreate CART database for CAVA and gff3 output',
     url = '...',
-    author = 'Marton Munz',
-    author_email = 'munzmarci@gmail.com',
+    author = 'RahmanTeam',
+    author_email = 'rahmanlab@icr.ac.uk',
     license = 'MIT',
     packages=['cartdb'],
     scripts=['bin/CARTdb.py','bin/cartdb'],
